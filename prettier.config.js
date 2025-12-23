54ff68e53081bf39ead09ca9b646d23118296242
Rhseung <ryu@rhseung.me>
2025-12-23 20:44:46 +0900

    ci: prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -5,6 +5,11 @@ const config = {
   semi: false,
   singleQuote: true,
   trailingComma: "all",
+  plugins: [
+    'prettier-plugin-tailwindcss',
+  ],
+  tailwindFunctions: ['clsx', 'cn', 'twMerge', 'tv', 'cva'],
+  tailwindPreserveWhiteSpace: false
 };
 
 export default config;
